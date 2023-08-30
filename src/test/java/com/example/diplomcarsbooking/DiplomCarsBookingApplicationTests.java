42300f44608c776b8cd159967e6f8f174469de3f
ArtyomKhodas <a.Khodas1601@gmail.com>
2023-08-30 11:04:02 +0300

    initial commit

---
+++ src/test/java/com/example/diplomcarsbooking/DiplomCarsBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.diplomcarsbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiplomCarsBookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
